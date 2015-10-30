d9809a22fc0ee66563499426925846e594931ab5
Michael Leung <leumi1@umbc.edu>
2015-10-30 16:39:08 -0400

    Commented js/mouse.js.

--- js/mouse.js
+++ js/mouse.js
@@ -1,6 +1,10 @@
+/*
+Handles detailed mouse events from the base onMouseMove/onTouchMove.
+*/
 (function(exports){
 
-	// Singleton
+	// Definition of mouse struct.
+	// Contains position vector and a LMB-down boolean.
 	var Mouse = {
 		x: 0,
 		y: 0,
@@ -8,37 +12,49 @@
 	};
 	exports.Mouse = Mouse;
 	
-	// Event Handling
+	// Events!
 	var onMouseMove,onTouchMove;
 	
+	//Mouse down event.
+	//If triggered, LMB must be pressed; update state singleton accordingly
+	//and pass through to onMouseMove.
 	document.body.addEventListener("mousedown",function(event){
 	    Mouse.pressed = true;
 	    onMouseMove(event);
 	},false);
 
+	//Mouse up event.
+	//If triggered, LMB must be released; update state singleton accordingly.
 	document.body.addEventListener("mouseup",function(event){
 	    Mouse.pressed = false;
 	},false);
 
+	//Mouse move event.
+	//If triggered, mouse position has changed.
 	document.body.addEventListener("mousemove",onMouseMove = function(event){
 		Mouse.x = event.pageX - window.scrollX;
 		Mouse.y = event.pageY - window.scrollY;
-
 	},false);
 
+	//Touch start event.
+	//If triggered, LMB must be pressed.
 	document.body.addEventListener("touchstart",function(event){
 	    Mouse.pressed = true;
 	    onTouchMove(event);
 	},false);
 
+	//Touch end event.
+	//If triggered, LMB must be released.
 	document.body.addEventListener("touchend",function(event){
 	    Mouse.pressed = false;
 	},false);
 
+	//Touch move event.
+	//If triggered, mouse position has changed.
 	document.body.addEventListener("touchmove",onTouchMove = function(event){
 		Mouse.x = event.changedTouches[0].clientX - window.scrollX;
 		Mouse.y = event.changedTouches[0].clientY - window.scrollY;
 	},false);
 
-
+//Set this as a globally-accessible function.
 })(window);
